34bb1179f79ea75c61e83e5728c2bde4031ed7d8
Nicolas Myers <nicolas@studiomyers.co.uk>
2011-05-11 22:14:43 +0100

    new routing system , new model

--- app/config/config.php
+++ app/config/config.php
@@ -1,23 +1,24 @@
 <?php 
-/*
- * --------------------------------------------------------------------
+
+/* --------------------------------------------------------------------
  * Configuration
  * --------------------------------------------------------------------
- *
  */
 
-$cfg['theme']     = 'default';
+$cfg['theme']           = 'default';
 
-$cfg['username']  = 'admin';
+$cfg['username']        = 'admin';
 
-$cfg['password']  = 'e3274be5c857fb42ab72d786e281b4b8';
+$cfg['password']        = 'e3274be5c857fb42ab72d786e281b4b8';
 
-$cfg['default_ctrl']  = 'index';
+$cfg['default_ctrl']    = 'index';
 
-//$cfg['base_url']  = "http://localhost/microfolio";
+//$cfg['base_url']      = "http://localhost/microfolio";
 
-//$cfg['base_dir']  = "/microfolio/";
+//$cfg['base_dir']      = "/microfolio/";
 
-$cfg['base_index'] = "";
+$cfg['cache_images']    = true;
 
+$cfg['image_quality']   = 90;
 
+$cfg['allowed_tags']    = '<p><h1><h2><h3><em><strong><a><br>';
--- app/model-n.php
+++ app/model-n.php
@@ -1,18 +1,21 @@
 <?php
 
 
-global $projects;
-$projects = Projects::singleton();
 
 class Projects implements Iterator {
 
     private static $instance;
     private $position = 0;
-    private $settings;
-    private $projects;
+
+    public static $folder = 'content/';
+    private $jsonFile = 'content.json';
+
+    public $settings;
+    public $projects;
 
     private function __construct() {
         $this->position = 0;
+        $this->load();
     }
 
     public static function singleton() {
@@ -20,7 +23,6 @@ class Projects implements Iterator {
             $c = __CLASS__;
             self::$instance = new $c;
         }
-
         return self::$instance;
     }
 
@@ -35,21 +37,52 @@ class Projects implements Iterator {
      *
      */
     public function load() {
-
+        $data = json_decode(file_get_contents(Projects::$folder.$this->jsonFile));
+        foreach ($data as $key => $value) {
+            if (is_object($value)) continue;
+            if (property_exists($this,$key)) $this->$key = $value;
+        }
+        foreach ($data->projects as $project_slug => $project) {
+            if (file_exists(Projects::$folder.$project_slug))
+                $this->projects[$project_slug] = new Project($project);
+        }
     }
 
     /**
      * Converts the projects back to json and saves the file
      */
     public function save() {
-
+        if (!rename(Projects::$folder.$this->jsonFile,Projects::$folder.$this->jsonFile.'.bak'))
+                throw new Exception("Can't rename json file.");
+        if (!file_put_contents(Projects::$folder.$this->jsonFile, $this->toJSON()))
+                throw new Exception("Can't write json file.");
+        return true;
     }
 
     /**
-     * Synchronizes the content folder with the json file
+     * Synchronizes the object with the actual content of the projects folder
+     *
      */
     public function sync() {
-
+        $files = scandir(Projects::$folder);
+        $project_slugs = array_keys($this->projects);
+        foreach ($files as $file) {
+            $isNew = $file != '.' && $file != '..';
+            $isNew = $isNew && is_dir(Projects::$folder . '/' . $file);
+            $isNew = $isNew && !in_array($file, $project_slugs);
+            if ($isNew) {
+                $this->projects[$file] = new Project(array(
+                    "timestamp" => time(),
+                    "title"     => $file,
+                    "slug"      => $file,
+                    "status"    => Project::PROJECT_OFFLINE,
+                    "parent"    => "root",
+                ));
+            }
+        }
+        //sync projects
+        foreach ($this->projects as $project)
+                $project->sync();
     }
 
     /**
@@ -57,8 +90,17 @@ class Projects implements Iterator {
      *
      * @param boolean $filter Removes hidden and offline projects if set to true
      */
-    public function getHTMLMenu($filter=true) {
-
+    public function getHTMLMenu($filter=true,$level='root') {
+        $html = "<ol>\n";
+        foreach ($this->projects as $key => $project) {
+            if ($project->parent == $level) {
+                $html .= "<li><div>".$project->title."</div>\n";
+                $html .= $this->getHTMLMenu($filter,$key);
+                $html .= "</li>\n";
+            }
+        }
+        $html .= "</ol>\n";
+        return $html;
     }
 
     /**
@@ -67,8 +109,18 @@ class Projects implements Iterator {
      *
      * @param array $array
      */
-    public function reorderProjects($array) {
-
+    public function reorderProjects($new_order) {
+        $tmp_projects = array();
+        if (count($new_order)!=count($this->projects))
+               throw new Exception("Error reordering projects, counts don't match.");
+        foreach ($new_order as $slug => $parent) {
+            if (isset ($this->projects[$slug])) {
+                $tmp_projects[$slug] = $this->projects[$slug];
+                $this->projects[$slug]->parent = $parent;
+            } else {
+                throw new Exception("Error reordering projects, unknown: '$slug'.");
+            }
+        }
     }
 
     /**
@@ -77,7 +129,9 @@ class Projects implements Iterator {
      * @param string $projectSlug
      */
     public function getProject($projectSlug) {
-
+        if (isset ($this->projects[$projectSlug]))
+            return $this->projects[$projectSlug];
+        throw new Exception("Cannot find project '$projectSlug'."); 
     }
 
     /**
@@ -86,7 +140,12 @@ class Projects implements Iterator {
      * @param string $projectTitle
      */
     public function addProject($projectTitle) {
-
+        $projectSlug = normalize_str($projectTitle);
+        if (!isset ($this->projects[$projectSlug])) {
+            $this->projects[$projectSlug] = new Project();
+            $this->projects[$projectSlug]->create($projectTitle);
+        }
+        throw new Exception("Cannot add '$projectTitle', project already exists.");
     }
 
     /**
@@ -95,14 +154,22 @@ class Projects implements Iterator {
      * @param string $projectSlug
      */
     public function deleteProject($projectSlug) {
+        if (isset ($this->projects[$projectSlug])) {
+            unset($this->projects[$projectSlug]);
+            return true;
+        }
+        throw new Exception("Cannot delete '$projectTitle', project does not exist.");
+    }
 
+    public function renameProject($oldProjectSlug,$newProjectSlug) {
+        //todo
     }
 
     /**
      * Converts this object to JSON
      */
     public function toJSON() {
-        
+        return indentJSON(json_encode($this));
     }
 
     //------------------------
@@ -137,40 +204,120 @@ class Projects implements Iterator {
 
 class Project implements Iterator {
 
+    const PROJECT_OFFLINE = 'offline';
+    const PROJECT_HIDDEN  = 'hidden';
+    const PROJECT_ONLINE  = 'online';
+
+    const ITEM_TYPE_EMBED = 'embed';
+    const ITEM_TYPE_IMAGE = 'image';
+
     private $position = 0;
-    private $title;
-    private $status;
-    private $parent;
-    private $style;
-    private $text;
-    private $gallery;
+    private $loaded = false;
 
-    public function __construct() {
+    public $slug;
+
+    public $title;
+    public $status;
+    public $timestamp;
+    public $parent;
+    public $style;
+    public $text;
+
+    public $gallery;
+
+    public function __construct($data=null) {
         $this->position = 0;
+        if ($data!=null) $this->load($data);
     }
 
-    public function load() {
-        
+    public function load($data) {
+        foreach ($data as $key => $value) {
+            if (is_object($value)) continue;
+            if (property_exists($this,$key)) $this->$key = $value;
+        }
+        if (isset($data->gallery)) {
+            foreach ($data->gallery as $uid => $item) {
+                if ($item->type!=Project::ITEM_TYPE_EMBED) {
+                   if (file_exists(Projects::$folder.  $this->slug.'/'.$item->url))
+                       $this->gallery[$uid] = new GalleryItem($item);
+                } else {
+                   $this->gallery[$uid] = new GalleryItem($item);
+                }
+            }
+        }
+        $this->loaded = true;
     }
 
-    public function reorder($array) {
-        
+    public function create($title) {
+        if (!$this->loaded)
+                throw new Exception("Project already loaded, cannot create '$title'.");
+        $this->title = $title;
+        $this->slug = normalize_str($title);
+        $this->timestamp = time();
+        if (!mkdir(Projects::$folder.$this->slug))
+                throw new Exception("Cannot create new folder: '".Projects::$folder.$this->slug."' .");
+    }
+
+    public function reorder($new_order) {
+        $new_gallery = array();
+        if (count($new_gallery)!=count($this->gallery))
+                throw new Exception("Cannot reorder. Item counts dont match.");
+        foreach ($new_order as $uid) {
+            if (isset($this->gallery[$uid]))
+                $new_gallery[$uid] = $this->gallery[$uid];
+            else
+                throw new Exception("Cannot reorder. Item uid does not exist: $uid.");
+        }
+
+    }
+    
+    public function sync() {
+        //add new image files
+        $folder = Projects::$folder.$this->slug.'/';
+        $files = scandir($folder);
+        $items = array();
+        if (isset($this->gallery)) {
+            foreach ($this->gallery as $item){
+                if ($item->type!=Project::ITEM_TYPE_EMBED) {
+                    $items[] = $item->url;
+                }
+            }
+        }
+        foreach ($files as $file) {
+            $isNew = is_file($folder. $file);
+            $isNew = $isNew && stripos($file, '.jpg');
+            $isNew = $isNew && !in_array($file, $items);
+            if ($isNew) {
+                $this->addItem(array(
+                    "type" => Project::ITEM_TYPE_IMAGE,
+                    "url" => $file
+                ));
+            }
+        }
     }
 
     public function getItem($uid) {
-        
+        if (isset($this->gallery[$uid]))
+            return $this->gallery[$uid];
+        throw new Exception("Cannot find item '$uid'.");
     }
 
-    public function addItem() {
-        
+    public function addItem($data) {
+        //the url/file need to exists.
+        if ($data['type']!=Project::ITEM_TYPE_EMBED)
+            if (!file_exists(Projects::$folder.$this->slug.'/'.$data['url']))
+                throw new Exception("Cannot add item, file does not exist: ".$data['url'].".");
+        $uid = uniqid('item_');
+        $this->gallery[$uid] = new GalleryItem($data);
+        return $this->gallery[$uid];
     }
 
     public function deleteItem($uid) {
-        
-    }
-
-    public function toJSON() {
-        
+        if (isset($this->gallery[$uid])) {
+            unset($this->gallery[$uid]);
+            return true;
+        }
+        throw new Exception("Cannot find item '$uid'.");
     }
 
     //------------------------
@@ -206,22 +353,74 @@ class Project implements Iterator {
 
 class GalleryItem {
 
-    private $uid;
-    private $type;
-    private $title;
-    private $caption;
-    private $url;
+    public $type;
+    public $title;
+    public $caption;
+    public $url;
 
-    public function __construct($uid, $type, $url, $title, $caption) {
-        $this->uid = $uid;
-        $this->type = $type;
-        $this->url = $url;
-        $this->title = $title;
-        $this->caption = $caption;
+    public function __construct($data) {
+        foreach ($data as $key => $value) {
+            if (property_exists($this,$key)) $this->$key = $value;
+        }
     }
 
-    public function toJSON() {
+}
 
+
+
+function normalize_str($rawTag) {
+     $accent  =". ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ";
+     $noaccent="--aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyyby";
+     $tag=strtr(trim($rawTag),$accent,$noaccent);
+     $normalized_valid_chars = 'a-zA-Z0-9-';
+     $normalized_tag = preg_replace("/[^$normalized_valid_chars]/", "", $tag);
+     return strtolower($normalized_tag);
+}
+
+function indentJSON($json) {
+
+    $result = '';
+    $pos = 0;
+    $strLen = strlen($json);
+    $indentStr = '    ';
+    $newLine = "\n";
+    $prevChar = '';
+    $outOfQuotes = true;
+
+    for ($i = 0; $i <= $strLen; $i++) {
+
+// Grab the next character in the string
+        $char = substr($json, $i, 1);
+
+// Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+        }
+// If this character is the end of an element,
+// output a new line and indent the next line
+        else if (($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos--;
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+// Add the character to the result string
+        $result .= $char;
+
+// If the last character was the beginning of an element,
+// output a new line and indent the next line
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos++;
+            }
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+        $prevChar = $char;
     }
 
+    return $result;
 }
