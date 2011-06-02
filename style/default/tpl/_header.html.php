8bfc8b1439ec409ca0d9dbb1b0371646561f6ff7
Nicolas Myers <nicolas@studiomyers.co.uk>
2011-06-02 17:14:22 +0200

    clean up

---
+++ style/default/tpl/_header.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <!-- metas -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <!-- css -->
+    <?=includeCSS("style.css")?>
+
+    <title><?=$project->title?></title>
+</head>
+    <body>
+    <?=$menu?>
+        <div id="content" >
